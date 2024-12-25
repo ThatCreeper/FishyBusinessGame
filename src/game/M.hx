591bde0fda87fad488d0362e2718631b98a403d5
ThatCreeper <creeperhostanimations@gmail.com>
2024-12-25 15:28:57 -0600

    Add M.rng

--- src/game/M.hx
+++ src/game/M.hx
@@ -4,4 +4,8 @@ class M {
     public static function lerp(from: Float, to: Float, x: Float) {
         return from + (to - from) * x;
     }
+
+    public static function rng(min: Float, max: Float) {
+        return lerp(min, max, Math.random());
+    }
 }
