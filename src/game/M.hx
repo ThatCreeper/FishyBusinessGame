0a2452d34a67bff08a281c4817d277e5a98d548d
ThatCreeper <creeperhostanimations@gmail.com>
2024-12-26 18:29:10 -0600

    Rectangle overlap

--- src/game/M.hx
+++ src/game/M.hx
@@ -12,4 +12,9 @@ class M {
     public static function clamp(x: Float, min: Float, max: Float) {
         return Math.min(Math.max(min, x), max);
     }
+
+    public static function rectsOverlap(x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float, x4: Float, y4: Float) {
+        return x1 <= x4 && x2 >= x3 &&
+               y1 <= y4 && y2 >= y3;
+    }
 }
